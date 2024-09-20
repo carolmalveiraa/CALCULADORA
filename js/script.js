796a5f540da90a6b14c7ae7b217ec796683cff97
Carol Malveira <carolmalveiraa12@icloud.com>
2024-09-19 22:42:20 -0300

    Ajustando falta de colchetes que apresentavam erro no código

--- js/script.js
+++ js/script.js
@@ -3,99 +3,141 @@ const currentOperationText = document.querySelector("#current-operation");
 const button = document.querySelectorAll("#button-container button");
 
 class Calculator {
-    constructor(previousOperationText, currentOperationText) {
-        this.previousOperationText = previousOperationText;
-        this.currentOperationText = currentOperationText;
-        this.currentOperation = "";
+  constructor(previousOperationText, currentOperationText) {
+    this.previousOperationText = previousOperationText;
+    this.currentOperationText = currentOperationText;
+    this.currentOperation = "";
+  }
+
+  // Adicionando números ao display
+  addDigit(digit) {
+    // Checando operção atual
+    if (digit === "." && this.currentOperationText.innerText.includes(".")) {
+      return;
     }
 
-    // Adicionando números ao display
-    addDigit(digit) {
-        // Checando operção atual
-        if(digit === "." && this.currentOperationText.innerText.includes(".")){
+    this.currentOperation = digit;
+    this.updateScreen();
+  }
+
+  // Novo método para processar operações
+  processOperation(operation) {
+    // Checando se há um número no display
+    if (this.currentOperationText.innerText === "" && operation !== "C") {
+      if (this.previousOperationText.innerText !== "") {
+        this.changeOperation(operation);
+      }
+      return;
+    }
+
+    // Antes
+    let operationValue;
+    let previous = +this.previousOperationText.innerText.split(" ")[0];
+    let current = +this.currentOperationText.innerText;
+
+    switch (operation) {
+      case "+":
+        operationValue = previous + current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+      case "-":
+        operationValue = previous - current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+      case "*":
+        operationValue = previous * current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+      case "÷":
+        operationValue = previous / current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+      case "DEL":
+        this.processDelOperator();
+        break;
+      case "CE":
+        this.processClearCurrentlOperator();
+        break;
+      case "C":
+        this.processClearOperator();
+        break;
+      case "=":
+        this.processEqualOperator();
+        break;
+      default:
         return;
-        }
+    }
+  }
 
-        this.currentOperation = digit;
-        this.updateScreen();
+  // Atualizando o display
+  updateScreen(
+    operationValue = null,
+    operation = null,
+    current = null,
+    previous = null
+  ) {
+    if (operationValue === null) {
+      // Append number to current value
+      this.currentOperationText.innerText += this.currentOperation;
+    } else {
+      // Check if value is zero, if is just add current value
+      if (previous === 0) {
+        operationValue = current;
+      }
+      // Add current value to previous
+      this.previousOperationText.innerText = `${operationValue} ${operation}`;
+      this.currentOperationText.innerText = "";
+    }
+  }
+
+  // Change math operation
+  changeOperation(operation) {
+    const mathOperations = ["*", "-", "+", "/"];
+
+    if (!mathOperations.includes(operation)) {
+      return;
     }
 
-    // Novo método para processar operações
-    processOperation(operation) {
+    this.previousOperationText.innerText =
+      this.previousOperationText.innerText.slice(0, -1) + operation;
+  }
 
-        // Checando se há um número no display
-        if (this.currentOperationText.innerText === "" && operation !== "C") {
-            if (this.previousOperationText.innerText !== "") {
-                this.changeOperation(operation);
-            }
-            return;
-        }
+  // Delete a digit
+  processDelOperator() {
+    this.currentOperationText.innerText =
+      this.currentOperationText.innerText.slice(0, -1);
+  }
 
-        // Antes
-        let operationValue;
-        let previous = +this.previousOperationText.innerText.split(" ")[0];
-        let current = +this.currentOperationText.innerText;
+  // Clear current operation
+  processClearCurrentOperator() {
+    this.currentOperationText.innerText = "";
+  }
 
-        switch(operation) {
-            case "+":
-                operationValue = previous + current;
-                this.updateScreen(operationValue, operation, current, previous);
-                break;
-            case "-":
-                operationValue = previous - current;
-                this.updateScreen(operationValue, operation, current, previous);
-                break;
-            case "*":
-                operationValue = previous * current;
-                this.updateScreen(operationValue, operation, current, previous);
-                break;
-            case "÷":
-                operationValue = previous / current;
-                this.updateScreen(operationValue, operation, current, previous);
-                break;
-            case "DEL":
-                this.processDelOperator();
-                break;
-            case "CE":
-                this.processClearCurrentlOperator();
-                break;
-            case "C":
-                this.processClearOperator();
-                break;
-            case "=":
-                this.processEqualOperator();
-                break;
-                default:
-                    return;
-        }
-    }
+  // Clear all operations
+  processClearOperator() {
+    this.currentOperationText.innerText = "";
+    this.previousOperationText.innerText = "";
+  }
 
-    // Atualizando o display
-    updateScreen(
-        operationValue = null, 
-        operation = null,
-        current = null,
-        previous = null
-    ) {
-        if (operantionValue === null ) {
-            this.currentOperationText.innerText += this.currentOperation;
-        }
-        else {
-            this.currentOperationText.innerText = operationValue;
-            this.previousOperationText.innerText = `${previous} ${operation} ${current}`;
-        }
+  // Process an operation
+  processEqualOperator() {
+    let operation = this.previousOperationText.innerText.split(" ")[1];
+
+    this.processOperation(operation);
+  }
 }
 
 const calc = new Calculator(previousOperationText, currentOperationText);
 
 buttons.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        const value = e.target.innerText;
+  btn.addEventListener("click", (e) => {
+    const value = e.target.innerText;
 
-        if(+value >= 0 || value === ".") {
-            calc.addDigit(value);
-        } else {
-            calc.processOperation(value);
-        }
-    });
+    if (+value >= 0 || value === ".") {
+      console.log(value);
+      calc.addDigit(value);
+    } else {
+      calc.processOperation(value);
+    }
+  });
 });

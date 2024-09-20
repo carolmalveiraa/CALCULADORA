3ddf9a0302b622dde9fa369e1dcdc42fc5bee258
Carol Malveira <carolmalveiraa12@icloud.com>
2024-09-19 22:00:03 -0300

    Integrando JavaScript a nossa calculadora

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const button = document.querySelectorAll("#button-container button");
